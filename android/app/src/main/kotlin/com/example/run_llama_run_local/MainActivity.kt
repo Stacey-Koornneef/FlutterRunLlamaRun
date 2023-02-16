4b67a54f4db0a94f9ea5f94c33203a49e1f41e62
Stacey Koornneef <stacey.koornneef@uoit.net>
2023-02-15 22:03:20 -0500

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/run_llama_run_local/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.run_llama_run_local
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -6,9 +6,37 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<camera_avfoundation/CameraPlugin.h>)
+#import <camera_avfoundation/CameraPlugin.h>
+#else
+@import camera_avfoundation;
+#endif
+
+#if __has_include(<image_picker_ios/FLTImagePickerPlugin.h>)
+#import <image_picker_ios/FLTImagePickerPlugin.h>
+#else
+@import image_picker_ios;
+#endif
+
+#if __has_include(<learning_text_recognition/LearningTextRecognitionPlugin.h>)
+#import <learning_text_recognition/LearningTextRecognitionPlugin.h>
+#else
+@import learning_text_recognition;
+#endif
+
+#if __has_include(<simple_ocr_plugin/SimpleOcrPlugin.h>)
+#import <simple_ocr_plugin/SimpleOcrPlugin.h>
+#else
+@import simple_ocr_plugin;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [CameraPlugin registerWithRegistrar:[registry registrarForPlugin:@"CameraPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [LearningTextRecognitionPlugin registerWithRegistrar:[registry registrarForPlugin:@"LearningTextRecognitionPlugin"]];
+  [SimpleOcrPlugin registerWithRegistrar:[registry registrarForPlugin:@"SimpleOcrPlugin"]];
 }
 
 @end
